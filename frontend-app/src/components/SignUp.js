e3d51295b1deca23d6d78218926baba0744b8493
Shivam <p.shivam1097@gmail.com>
2024-08-03 15:12:07 +0530

    my first commit

--- frontend-app/src/components/SignUp.js
+++ frontend-app/src/components/SignUp.js
@@ -1,121 +1,70 @@
 import React, { useState } from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useMutation } from '@apollo/client';
 import { REGISTER_USER } from '../graphql/mutations';
 import { useNavigate } from 'react-router-dom';
 import './styles/SignUp.css';
 
-function Copyright(props) {
+function Copyright() {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Sharif © '}
-      <Link color="inherit" href="https://mui.com/">
-         Finance
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
+    <footer className="copyright">
+      <p>Sharif © <a href="https://mui.com/">Finance</a> {new Date().getFullYear()}.</p>
+    </footer>
   );
 }
 
-const defaultTheme = createTheme();
-
 export default function SignUp() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [registerUser] = useMutation(REGISTER_USER);
-    const navigate = useNavigate();
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-          const { data } = await registerUser({ variables: { username, password } });
-          if (data) {
-            console.log("Registration successful", data);
-            localStorage.setItem('token', data.registerUser.token);
-            navigate('/signin');
-          } else {
-            console.error("Registration failed, no data returned");
-          }
-        } catch (error) {
-          console.error("Registration error:", error);
-        }
-      };
-  
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [registerUser] = useMutation(REGISTER_USER);
+  const navigate = useNavigate();
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await registerUser({ variables: { username, password } });
+      if (data) {
+        console.log("Registration successful", data);
+        localStorage.setItem('token', data.registerUser.token);
+        navigate('/signin');
+      } else {
+        console.error("Registration failed, no data returned");
+      }
+    } catch (error) {
+      console.error("Registration error:", error);
+    }
+  };
 
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs" className="signup-container">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-         <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <AccountCircleIcon />
-              </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-              label="Username"
-              name="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container>
-              <Grid item xs>
-              </Grid>
-              <Grid item>
-                <Link  href="/signin" variant="body2">
-                  {"Already have an account? Sign In"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
-  );
+    <div className="signup-container">
+      <div className="signup-box">
+        <div className="avatar">
+          <span className="avatar-icon">A</span>
+        </div>
+        <h1 className="title">Sign Up</h1>
+        <form onSubmit={handleSubmit} className="signup-form">
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit" className="submit-button">Sign Up</button>
+          <div className="signin-link">
+            <a href="/signin">Already have an account? Sign In</a>
+          </div>
+        </form>
+        <Copyright />
+      </div>
+    </div>
+  );
 }
