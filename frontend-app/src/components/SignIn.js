2330c84b5241c3dc471ac84834859ec5bd8e0863
Shivam <p.shivam1097@gmail.com>
2024-08-03 15:00:54 +0530

    my first commit

--- frontend-app/src/components/SignIn.js
+++ frontend-app/src/components/SignIn.js
@@ -2,24 +2,7 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../graphql/mutations';
 import { useNavigate } from 'react-router-dom';
-import { Box, Button, Container, CssBaseline, TextField, Typography, Grid, Link, Avatar } from '@mui/material';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './styles/SignIn.css';
-
-function Copyright(props) {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Sharif © '}
-      <Link color="inherit" href="https://mui.com/">
-         Finance
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-const theme = createTheme();
 
 export default function SignIn() {
   const [username, setUsername] = useState('');
@@ -39,68 +22,38 @@ export default function SignIn() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs" className="signup-container">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-         <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <AccountCircleIcon />
-              </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign In
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-              label="Username"
-              name="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-              </Grid>
-              <Grid item>
-                <Link  href="/signup" variant="body2">
-                  {"Create an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
-  );
+    <div className="signin-container">
+      <div className="signin-box">
+        <div className="avatar">
+          <span className="avatar-icon">A</span>
+        </div>
+        <h1 className="title">Sign In</h1>
+        <form onSubmit={handleSubmit} className="signin-form">
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <button type="submit" className="submit-button">Sign In</button>
+        </form>
+        <div className="signup-link">
+          <a href="/signup">Create an account? Sign Up</a>
+        </div>
+        <footer className="copyright">
+          <p>copyright © <a href="https://mui.com/">Finance</a> {new Date().getFullYear()}.</p>
+        </footer>
+      </div>
+    </div>
+  );
 }
